97332c0d8026772a3b8224dc735ef7710eec8fc7
McLutzifer <lukasschumi@gmx.at>
2021-08-11 15:15:19 +0200

    collections

--- learnxinyminutes.ts
+++ learnxinyminutes.ts
@@ -13,3 +13,23 @@ let anotherName = "Anders";
 let notSure: any = 4;
 notSure = "maybe a string instead";
 notSure = false;  // okay ,definitely a boolean
+
+// Use const keyword for constants
+const numLivesForCat = 9;
+//numLivesForCat = 1 ... Error
+
+// For collections, there are typed arrays and generic arrays
+let list: number[] = [1, 2, 3];
+// Alternatively, using the generic array type
+let newList: Array<number> = [1, 2, 3];
+let otherList: Array<string> = ['peter', 'paul', 'mary'];
+
+// For enumerations:
+enum Color { Red, Green, Blue };
+let c: Color = Color.Green;
+
+// Lastly, "void" is used in the special case of a function returning nothing
+function bigHorribleAlert(): void {
+    alert("I#m a little annoying box!");
+}
+}
