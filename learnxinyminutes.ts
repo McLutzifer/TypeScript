7dc783ff36df91626a2c135bbd23d60f38a5bd55
McLutzifer <lukasschumi@gmx.at>
2021-08-11 15:08:50 +0200

    omit type annotation

--- learnxinyminutes.ts
+++ learnxinyminutes.ts
@@ -2,3 +2,10 @@
 let isDone: boolean = false;
 let lines: number = 42;
 let myName : string = "Anders"
+
+// But you can omit the type annotation if the variables are derived
+// from explicit literals
+let isNotDone = false;
+let numberOfLines = 42;
+let anotherName = "Anders";
+
