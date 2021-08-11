49fc315e4dcbe39bc85ae204bffb5f2ea3e2133b
McLutzifer <lukasschumi@gmx.at>
2021-08-11 20:47:33 +0200

    parameter types

--- learnxinyminutes.ts
+++ learnxinyminutes.ts
@@ -63,3 +63,15 @@ interface Person {
 // Object that implements the "Person" interface
 // Can be treated as a person since it has the name and move properties
 let p: Person = { name: "Bobby", move: () => { } }
+// Objects that have the optional property:
+let validPerson: Person = { name: "Bobby", age: 42, move: () => { } };
+
+// INterfaces can also describe a function type
+interface SearchFunc {
+    (source: string, subString: string): boolean;
+}
+// Only the parameters' types are important, names are not important.
+let mySearch: SearchFunc;
+mySearch = function (src: string, sub: string) {
+    return src.search(sub) != -1;
+}
